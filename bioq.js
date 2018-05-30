994b4fa668c6923b70b74660682c65ad512ed4ec
KV <komal.vadnagara@gmail.com>
2018-05-29 20:30:24 -0700

    updated tabs

---
+++ bioq.js
@@ -0,0 +1,10 @@
+// Signup submit 
+$('#gform').on('submit', function(e){
+  	$('#gform *').fadeOut(1);
+  	$('#gform').prepend('Thanks! Stay quilty....');
+});
+
+//close the modal with x 
+$('.cancel-icon').click(function(){
+    $('.bs-example-modal-sm').modal('hide');
+});
