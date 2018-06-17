8dc0c8990aad58c1d88a813c06af251c42bc1de7
KV <komal.vadnagara@gmail.com>
2018-06-17 11:45:16 -0700

    added scientific advisory board info

--- bioq.js
+++ bioq.js
@@ -9,3 +9,20 @@ $('.cancel-icon').click(function(){
     $('.bs-example-modal-sm').modal('hide');
 });
 
+//hover over scientific advisory board
+let sab1 = document.getElementById('parent1');
+sab1.onmouseover = function() {
+  document.getElementById('popup1').style.display = 'block';
+}
+sab1.onmouseout = function() {
+  document.getElementById('popup1').style.display = 'none';
+}
+
+let sab2 = document.getElementById('parent2');
+sab2.onmouseover = function() {
+  document.getElementById('popup2').style.display = 'block';
+}
+sab2.onmouseout = function() {
+  document.getElementById('popup2').style.display = 'none';
+}
+
